49507a4649a783edabdf1b4b566adce5fefe9e4c
fligthlich <fligthlich@gmail.com>
2020-11-30 07:19:57 +0100

    30/11/2020

--- Alexandre/FRONT/DYNAMIQUE/JAVASCRIPT/Javascript_pour_les_pages_web/exo2/javascript.js
+++ Alexandre/FRONT/DYNAMIQUE/JAVASCRIPT/Javascript_pour_les_pages_web/exo2/javascript.js
@@ -1,4 +1,3 @@
-
 // alert("<div>baliseHTML</div>");
 // console.log("Ah que Coucou !");
 // document.write("Coucou beuh !");
@@ -19,38 +18,53 @@
 // document.querySelector('#button2').onclick = function() 
 // {
 //     alert('Vous avez cliqué !');
-// }   
+// }  
+
+// Génération d'un nombre Aléatoire
 let nb_rand = Math.floor(Math.random() * 100 + 1);
 console.log(nb_rand);
-let value = document.getElementById('nom').value;
-let click = 0;
-function get_value(params) {
-    if (value < nb_rand)
-    {
-        alert("+")
-    }
-    else if (value > nb_rand) {
-        alert("-")
-    } else {
-        alert("=")
-    }
-    click++;
-    console.log(click);
 
+
+// Javascript intrusif (on le met dans le code html avec onclick="verif()")
+function verif(){
+    
+    let value = document.getElementById('nombre').value;
+    let label = document.getElementById('label1');
+
+    if (value < nb_rand) 
+    {
+        label.innerHTML = "Trop petit";
+    } 
+    else if (value > nb_rand)
+    {
+        label.innerHTML = "Trop grand";
+    } 
+    else
+    {
+        label.innerHTML = "C'est gagné";
+    }
 }
 
+// Javascript non intrusif (on ne le met pas dans le code html)
+let valeur = document.getElementById('button1');
+valeur.addEventListener("click",function(e){
+    
+    let value = document.getElementById('nombre').value;
+    let label = document.getElementById('label1');
 
-
-// let value = document.getElementById('nom').value;
-
-// document.addEventListener("click",function ()
-// {
-//     alert("Vous avez entrer " + value);  
-// });
-
-
-
-
+    if (value < nb_rand) 
+    {
+        label.innerHTML = "Trop petit";
+    } 
+    else if (value > nb_rand)
+    {
+        label.innerHTML = "Trop grand";
+    } 
+    else
+    {
+        label.innerHTML = "C'est gagné";
+    }
+});
 
 
 
